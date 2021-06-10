b327d8b3a802534a3dc6c73268b64383c6724182
juarce <juarce@itba.edu.ar>
2021-06-09 21:32:53 -0300

    Sign In view added.

--- app/src/main/java/ar/edu/itba/hci/profitapp/MainActivity.java
+++ app/src/main/java/ar/edu/itba/hci/profitapp/MainActivity.java
@@ -1,8 +1,13 @@
 package ar.edu.itba.hci.profitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        VideoView videoview = (VideoView) findViewById(R.id.videoView);
+//        Uri uri = Uri.parse("https://assets.mixkit.co/videos/preview/mixkit-man-training-by-the-sea-14065-large.mp4");
+//        videoview.setVideoURI(uri);
+//        videoview.start();
+        
     }
+
+
 }
