4ac5e9770f0f419640b80d72277af1819ac3b3bf
juarce <juarce@itba.edu.ar>
2021-06-15 16:51:42 -0300

    UserViewModel added. User binded with layout.

--- app/src/main/java/ar/edu/itba/hci/profitapp/ui/ProfileActivity.java
+++ app/src/main/java/ar/edu/itba/hci/profitapp/ui/ProfileActivity.java
@@ -4,13 +4,21 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import ar.edu.itba.hci.profitapp.App;
 import ar.edu.itba.hci.profitapp.databinding.ActivityProfileBinding;
+import ar.edu.itba.hci.profitapp.repository.RoutineRepository;
+import ar.edu.itba.hci.profitapp.repository.Status;
+import ar.edu.itba.hci.profitapp.repository.UserRepository;
+import ar.edu.itba.hci.profitapp.viewModel.RoutineViewModel;
+import ar.edu.itba.hci.profitapp.viewModel.UserViewModel;
+import ar.edu.itba.hci.profitapp.viewModel.repositoryVM.RepositoryViewModelFactory;
 
 public class ProfileActivity extends AppCompatActivity {
+    private App app;
     private ActivityProfileBinding profileBinding;
-    View view;
-    private TextView username;
+    private UserViewModel userViewModel;
 
 
     @Override
@@ -18,5 +26,20 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         profileBinding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(profileBinding.getRoot());
+
+        app = ((App) getApplication());
+
+        ViewModelProvider.Factory viewModelFactory = new RepositoryViewModelFactory<>(UserRepository.class, app.getUserRepository());
+        userViewModel = new ViewModelProvider(this, viewModelFactory).get(UserViewModel.class);
+
+        userViewModel.getCurrentUser().observe(this, r -> {
+            if (r.getStatus() == Status.SUCCESS) {
+                if(r.getData() != null && r.getData() != null) {
+                    profileBinding.setUser(r.getData());
+                }
+            } else {
+//                defaultResourceHandler(r);
+            }
+        });
     }
 }
--- app/src/main/java/ar/edu/itba/hci/profitapp/viewModel/UserViewModel.java
+++ app/src/main/java/ar/edu/itba/hci/profitapp/viewModel/UserViewModel.java
@@ -4,17 +4,30 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import org.jetbrains.annotations.NotNull;
 
+import ar.edu.itba.hci.profitapp.api.model.Routine;
 import ar.edu.itba.hci.profitapp.api.model.User;
+import ar.edu.itba.hci.profitapp.repository.Resource;
+import ar.edu.itba.hci.profitapp.repository.UserRepository;
+import ar.edu.itba.hci.profitapp.viewModel.repositoryVM.RepositoryViewModel;
 
-public class UserViewModel extends AndroidViewModel {
+public class UserViewModel extends RepositoryViewModel<UserRepository> {
 
-    private MutableLiveData<User> user = new MutableLiveData<>();
+//    private MutableLiveData<User> user = new MutableLiveData<>();
+    private LiveData<Resource<User>> currentUser;
 
-    public UserViewModel(@NonNull @NotNull Application application) {
-        super(application);
+    public UserViewModel(UserRepository repository) {
+        super(repository);
+    }
+
+    //TODO logout
+
+    public LiveData<Resource<User>> getCurrentUser() {
+        currentUser = repository.getCurrentUser();
+        return currentUser;
     }
 }
